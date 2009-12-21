2dd4b38dd1e9c6bc2f8c11149d706c0759cee9bf
Jonathan Hseu <vomjom@vomjom.net>
2009-12-20 18:52:17 -0600

    thread local connections

--- connection.py
+++ connection.py
@@ -1,31 +1,77 @@
+import threading
+from Queue import Queue
+
 from thrift import Thrift
 from thrift.transport import TTransport
 from thrift.transport import TSocket
 from thrift.protocol import TBinaryProtocol
 from cassandra import Cassandra
 
-__all__ = ['connect']
+__all__ = ['connect', 'connect_thread_local']
 
-def connect(host='localhost', port=9160):
+DEFAULT_SERVER = 'localhost:9160'
+
+def connect(server=DEFAULT_SERVER):
     """
     Construct a Cassandra connection
 
     Parameters
     ----------
-    host : str
-           The host of the Cassandra server
-    port : int
-           The port of the Cassandra server
+    server : str
+             Cassandra server with format: "hostname:port"
 
     Returns
     -------
     Cassandra client
     """
 
-    socket = TSocket.TSocket(host, port)
+    host, port = server.split(":")
+    socket = TSocket.TSocket(host, int(port))
     transport = TTransport.TBufferedTransport(socket)
     protocol = TBinaryProtocol.TBinaryProtocolAccelerated(transport)
     client = Cassandra.Client(protocol)
     transport.open()
 
     return client
+
+def connect_thread_local(servers=None):
+    """
+    Construct a Cassandra connection, one for each thread
+
+    Parameters
+    ----------
+    servers: [server]
+             List of Cassandra servers with format: "hostname:port"
+
+    Returns
+    -------
+    Cassandra client
+    """
+
+   if servers is None:
+        servers = [DEFAULT_SERVER]
+    return ThreadLocalConnection(servers)
+
+class ThreadLocalConnection(object):
+    def __init__(self, servers):
+        self.queue = Queue()
+        for server in servers:
+            self.queue.put(server)
+        self.local = threading.local()
+
+    def __getattr__(self, attr):
+        def client_call(*args, **kwargs):
+            if getattr(self.local, 'client', None) is None:
+                server = self.queue.get()
+                self.local.client = connect(server)
+                self.queue.put(server)
+
+            try:
+                return getattr(self.local.client, attr)(*args, **kwargs)
+            except Thrift.TException, exc:
+                # Connection error, try a new server next time
+                self.local.client = None
+                raise exc
+
+        setattr(self, attr, client_call)
+        return getattr(self, attr)
