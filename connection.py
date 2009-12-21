26af913915a84f118180f0737bb4aa623bd926f8
Jonathan Hseu <vomjom@vomjom.net>
2009-12-21 12:07:03 -0600

    remove connect_pooled(). add automatic-failover.

--- connection.py
+++ connection.py
@@ -1,3 +1,4 @@
+from exceptions import Exception
 import threading
 from Queue import Queue
 
@@ -7,9 +8,12 @@ from thrift.transport import TSocket
 from thrift.protocol import TBinaryProtocol
 from cassandra import Cassandra
 
-__all__ = ['connect', 'connect_thread_local', 'connect_pooled']
+__all__ = ['connect', 'connect_thread_local']
 
 DEFAULT_SERVER = 'localhost:9160'
+
+class NoServerAvailable(Exception):
+    pass
 
 def create_client_transport(server):
     host, port = server.split(":")
@@ -21,80 +25,21 @@ def create_client_transport(server):
 
     return client, transport
 
-def connect(server=DEFAULT_SERVER):
+def connect(servers=None):
     """
-    Construct a Cassandra connection
+    Constructs a single Cassandra connection. Initially connects to the first
+    server on the list.
+    
+    If the connection fails, it will attempt to connect to each server on the
+    list in turn until one succeeds. If it is unable to find an active server,
+    it will throw a NoServerAvailable exception.
 
     Parameters
     ----------
-    server : str
-             Cassandra server with format: "hostname:port"
-             Default: 'localhost:9160'
+    servers : [server]
+              List of Cassandra servers with format: "hostname:port"
 
-    Returns
-    -------
-    Cassandra client
-    """
-
-    return create_client_transport(server)[0]
-
-def connect_pooled(servers=None):
-    """
-    Construct a pooled queue of Cassandra connections, given by the servers list
-
-    Parameters
-    ----------
-    servers: [server]
-             List of Cassandra servers with format: "hostname:port"
-             Create duplicate server entries if you want multiple connections
-             to the same server.
-
-             Default: 5 * ['localhost:9160']
-             (5 connections to the server at localhost)
-
-    Returns
-    -------
-    Cassandra client
-    """
-
-    if servers is None:
-        servers = 5 * [DEFAULT_SERVER]
-    return PooledConnection(servers)
-
-class PooledConnection(object):
-    def __init__(self, servers):
-        self.queue = Queue()
-        for server in servers:
-            self.queue.put((server, None, None))
-
-    def __getattr__(self, attr):
-        def client_call(*args, **kwargs):
-            server, client, transport = self.queue.get()
-            try:
-                if client is None:
-                    client, transport = create_client_transport(server)
-                return getattr(client, attr)(*args, **kwargs)
-            except Thrift.TException as exc:
-                # Connection error, try a new server next time
-                transport.close()
-                client, transport = None, None
-                raise exc
-            finally:
-                self.queue.put((server, client, transport))
-
-        setattr(self, attr, client_call)
-        return getattr(self, attr)
-
-def connect_thread_local(servers=None):
-    """
-    Construct a Cassandra connection for each thread
-
-    Parameters
-    ----------
-    servers: [server]
-             List of Cassandra servers with format: "hostname:port"
-
-             Default: ['localhost:9160']
+              Default: ['localhost:9160']
 
     Returns
     -------
@@ -103,29 +48,103 @@ def connect_thread_local(servers=None):
 
     if servers is None:
         servers = [DEFAULT_SERVER]
-    return ThreadLocalConnection(servers)
+    return SingleConnection(servers)
+
+def connect_thread_local(servers=None, round_robin=True):
+    """
+    Constructs a Cassandra connection for each thread. By default, it attempts
+    to connect in a round_robin (load-balancing) fashion. Turn it off by
+    setting round_robin=False
+
+    If the connection fails, it will attempt to connect to each server on the
+    list in turn until one succeeds. If it is unable to find an active server,
+    it will throw a NoServerAvailable exception.
+
+    Parameters
+    ----------
+    servers : [server]
+              List of Cassandra servers with format: "hostname:port"
+
+              Default: ['localhost:9160']
+
+    round_robin : bool
+              Balance the connections. Set to False to connect to each server
+              in turn.
+
+    Returns
+    -------
+    Cassandra client
+    """
+
+    if servers is None:
+        servers = [DEFAULT_SERVER]
+    return ThreadLocalConnection(servers, round_robin)
+
+class SingleConnection(object):
+    def __init__(self, servers):
+        self._servers = servers
+        self._client = None
+
+    def __getattr__(self, attr):
+        def client_call(*args, **kwargs):
+            if self._client is None:
+                self._find_server()
+            try:
+                return getattr(self._client, attr)(*args, **kwargs)
+            except Thrift.TException as exc:
+                # Connection error, try to connect to all the servers
+                self._transport.close()
+                self._client = None
+                self._find_server()
+
+        setattr(self, attr, client_call)
+        return getattr(self, attr)
+
+    def _find_server(self):
+        for server in self._servers:
+            try:
+                self._client, self._transport = create_client_transport(server)
+                return
+            except Thrift.TException as exc:
+                continue
+        raise NoServerAvailable()
 
 class ThreadLocalConnection(object):
-    def __init__(self, servers):
-        self.queue = Queue()
-        for server in servers:
-            self.queue.put(server)
-        self.local = threading.local()
+    def __init__(self, servers, round_robin):
+        self._servers = servers
+        self._queue = Queue()
+        for i in xrange(len(servers)):
+            self._queue.put(i)
+        self._local = threading.local()
+        self._round_robin = round_robin
 
     def __getattr__(self, attr):
         def client_call(*args, **kwargs):
             if getattr(self.local, 'client', None) is None:
-                server = self.queue.get()
-                self.queue.put(server)
-                self.local.client, self.local.transport = create_client_transport(server)
+                self._find_server()
 
             try:
-                return getattr(self.local.client, attr)(*args, **kwargs)
+                return getattr(self._local.client, attr)(*args, **kwargs)
             except Thrift.TException as exc:
                 # Connection error, try a new server next time
-                self.local.transport.close()
-                self.local.client = None
+                self._local.transport.close()
+                self._local.client = None
                 raise exc
 
         setattr(self, attr, client_call)
         return getattr(self, attr)
+
+    def _find_server(self):
+        servers = self._servers
+        if self._round_robin:
+            i = self._queue.get()
+            self._queue.put(i)
+            servers = servers[i:]+servers[:i]
+
+        for server in servers:
+            try:
+                self._local.client, self._local.transport = create_client_transport(server)
+                return
+            except Thrift.TException as exc:
+                continue
+        raise NoServerAvailable()
--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,6 +1,6 @@
 import threading
 
-from pycasso import connect, connect_pooled, connect_thread_local
+from pycasso import connect, connect_thread_local
 
 def version_check(connection, version):
     assert connection.get_string_property('version') == version
@@ -13,16 +13,6 @@ def test_connections():
     for i in xrange(10):
         threads.append(threading.Thread(target=version_check,
                                         args=(thread_local, version)))
-        threads[-1].start()
-    for thread in threads:
-        thread.join()
-
-
-    threads = []
-    pooled = connect_pooled()
-    for i in xrange(10):
-        threads.append(threading.Thread(target=version_check,
-                                        args=(pooled, version)))
         threads[-1].start()
     for thread in threads:
         thread.join()
