63873d9890de3f8bb6e96b66f947abe5049b4cbc
Jonathan Hseu <vomjom@vomjom.net>
2010-06-21 22:44:08 -0700

    remove unit test that fails on new ColumnFamilyMap semantics

--- tests/test_columnfamilymap.py
+++ tests/test_columnfamilymap.py
@@ -67,13 +67,6 @@ class TestColumnFamilyMap:
 
         return instance
 
-    def test_will_not_insert_none(self):
-        for column in ('strcol', 'intcol', 'floatcol', 'datetimecol',
-                       'intstrcol', 'floatstrcol', 'datetimestrcol'):
-            instance = self.instance('TestColumnFamilyMap.test_will_not_insert_none')
-            setattr(instance, column, None)
-            assert_raises(TypeError, self.map.insert, instance)
-
     def test_empty(self):
         key = 'TestColumnFamilyMap.test_empty'
         assert_raises(NotFoundException, self.map.get, key)
