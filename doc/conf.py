218e3eff3901ab2c8599bfd859b1659eb5e2cc30
Tyler Hobbs <tyler@riptano.com>
2010-11-02 16:30:10 -0500

    Increment version in doc config file

--- doc/conf.py
+++ doc/conf.py
@@ -33,9 +33,9 @@ project = 'pycassa'
 # built documents.
 #
 # The short X.Y version.
-version = '0.5.1'
+version = '0.5.2'
 # The full version, including alpha/beta/rc tags.
-release = '0.5.1'
+release = '0.5.2'
 
 # List of documents that shouldn't be included in the build.
 unused_docs = []
