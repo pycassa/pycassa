5346d6297bb262bb00f54bf703985e8fc3624183
Tyler Hobbs <tyler@riptano.com>
2010-11-16 17:42:33 -0600

    Roll back setup.py and conf.py to 0.5.x version numbers

--- doc/conf.py
+++ doc/conf.py
@@ -33,9 +33,9 @@ project = 'pycassa'
 # built documents.
 #
 # The short X.Y version.
-version = '0.5.1'
+version = '0.5.3'
 # The full version, including alpha/beta/rc tags.
-release = '0.5.1'
+release = '0.5.3'
 
 # List of documents that shouldn't be included in the build.
 unused_docs = []
