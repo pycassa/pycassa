01eca00d3495eee11cd44b46c081eca7bfa781a5
Tyler Hobbs <tyler@riptano.com>
2010-11-16 18:43:20 -0600

    Version bump to 0.5.4 and changelog update

--- doc/conf.py
+++ doc/conf.py
@@ -33,9 +33,9 @@ project = 'pycassa'
 # built documents.
 #
 # The short X.Y version.
-version = '0.5.3'
+version = '0.5.4'
 # The full version, including alpha/beta/rc tags.
-release = '0.5.3'
+release = '0.5.4'
 
 # List of documents that shouldn't be included in the build.
 unused_docs = []
