a641020995a76948093d42091b45b7bf5ecd0c31
Tyler Hobbs <tyler@riptano.com>
2011-02-03 19:56:37 -0600

    More extensive super column tests

--- tests/test_columnfamily.py
+++ tests/test_columnfamily.py
@@ -4,7 +4,6 @@ from pycassa import index, ColumnFamily, ConsistencyLevel, ConnectionPool,\
 from nose.tools import assert_raises, assert_equal, assert_true
 from nose.plugins.skip import *
 
-import struct
 import unittest
 
 def setup_module():
@@ -12,7 +11,7 @@ def setup_module():
     credentials = {'username': 'jsmith', 'password': 'havebadpass'}
     pool = ConnectionPool(keyspace='PycassaTestKeyspace', credentials=credentials)
     cf = ColumnFamily(pool, 'Standard1', dict_class=TestDict)
-    scf = ColumnFamily(pool, 'Super1', dict_class=TestDict)
+    scf = ColumnFamily(pool, 'Super1', dict_class=dict)
     indexed_cf = ColumnFamily(pool, 'Indexed1')
     sys_man = SystemManager()
 
@@ -293,28 +292,197 @@ class TestColumnFamily(unittest.TestCase):
         cf.insert(key, {'1': 'val1'})
         assert isinstance(cf.get(key), TestDict)
 
-class TestSuperColumnFamily:
+class TestSuperColumnFamily(unittest.TestCase):
 
     def tearDown(self):
         for key, columns in scf.get_range():
             scf.remove(key)
 
-    def test_super(self):
-        key = 'TestSuperColumnFamily.test_super'
+    def test_empty(self):
+        key = 'TestSuperColumnFamily.test_empty'
+        assert_raises(NotFoundException, cf.get, key)
+        assert_equal(len(cf.multiget([key])), 0)
+        for key, columns in cf.get_range():
+            assert_equal(len(columns), 0)
+
+    def test_get_whole_row(self):
+        key = 'TestSuperColumnFamily.test_get_whole_row'
         columns = {'1': {'sub1': 'val1', 'sub2': 'val2'}, '2': {'sub3': 'val3', 'sub4': 'val4'}}
-        assert_raises(NotFoundException, scf.get, key)
         scf.insert(key, columns)
         assert_equal(scf.get(key), columns)
-        assert_equal(scf.multiget([key]), {key: columns})
-        assert_equal(list(scf.get_range(start=key, finish=key)), [(key, columns)])
 
-    def test_super_column_argument(self):
-        key = 'TestSuperColumnFamily.test_super_columns_argument'
-        sub12 = {'sub1': 'val1', 'sub2': 'val2'}
-        sub34 = {'sub3': 'val3', 'sub4': 'val4'}
-        columns = {'1': sub12, '2': sub34}
+    def test_get_super_column(self):
+        key = 'TestSuperColumnFamily.test_get_super_column'
+        subcolumns = {'sub1': 'val1', 'sub2': 'val2', 'sub3': 'val3'}
+        columns = {'1': subcolumns}
         scf.insert(key, columns)
-        assert_equal(scf.get(key, super_column='1'), sub12)
-        assert_raises(NotFoundException, scf.get, key, super_column='3')
-        assert_equal(scf.multiget([key], super_column='1'), {key: sub12})
-        assert_equal(list(scf.get_range(start=key, finish=key, super_column='1')), [(key, sub12)])
+        assert_equal(scf.get(key), columns)
+        assert_equal(scf.get(key, super_column='1'), subcolumns)
+        assert_equal(scf.get(key, super_column='1', columns=['sub1']),     {'sub1': 'val1'})
+        assert_equal(scf.get(key, super_column='1', column_start='sub3'),  {'sub3': 'val3'})
+        assert_equal(scf.get(key, super_column='1', column_finish='sub1'), {'sub1': 'val1'})
+        assert_equal(scf.get(key, super_column='1', column_count=1),       {'sub1': 'val1'})
+        assert_equal(scf.get(key, super_column='1', column_count=1, column_reversed=True), {'sub3': 'val3'})
+
+    def test_get_super_columns(self):
+        key = 'TestSuperColumnFamily.test_get_super_columns'
+        super1 = {'sub1': 'val1', 'sub2': 'val2'}
+        super2 = {'sub3': 'val3', 'sub4': 'val4'}
+        super3 = {'sub5': 'val5', 'sub6': 'val6'}
+        columns = {'1': super1, '2': super2, '3': super3}
+        scf.insert(key, columns)
+        assert_equal(scf.get(key), columns)
+        assert_equal(scf.get(key, columns=['1']),     {'1': super1})
+        assert_equal(scf.get(key, column_start='3'),  {'3': super3})
+        assert_equal(scf.get(key, column_finish='1'), {'1': super1})
+        assert_equal(scf.get(key, column_count=1),    {'1': super1})
+        assert_equal(scf.get(key, column_count=1, column_reversed=True), {'3': super3})
+
+    def test_multiget_supercolumn(self):
+        key1 = 'TestSuerColumnFamily.test_multiget_supercolumn1'
+        key2 = 'TestSuerColumnFamily.test_multiget_supercolumn2'
+        keys = [key1, key2]
+        subcolumns = {'sub1': 'val1', 'sub2': 'val2', 'sub3': 'val3'}
+        columns = {'1': subcolumns}
+        scf.insert(key1, columns)
+        scf.insert(key2, columns)
+
+        assert_equal(scf.multiget(keys),
+                     {key1: columns, key2: columns})
+
+        assert_equal(scf.multiget(keys, super_column='1'),
+                     {key1: subcolumns, key2: subcolumns})
+
+        assert_equal(scf.multiget(keys, super_column='1', columns=['sub1']),
+                     {key1: {'sub1': 'val1'}, key2: {'sub1': 'val1'}})
+
+        assert_equal(scf.multiget(keys, super_column='1', column_start='sub3'),
+                     {key1: {'sub3': 'val3'}, key2: {'sub3': 'val3'}})
+
+        assert_equal(scf.multiget(keys, super_column='1', column_finish='sub1'),
+                     {key1: {'sub1': 'val1'}, key2: {'sub1': 'val1'}})
+
+        assert_equal(scf.multiget(keys, super_column='1', column_count=1),
+                     {key1: {'sub1': 'val1'}, key2: {'sub1': 'val1'}})
+
+        assert_equal(scf.multiget(keys, super_column='1', column_count=1, column_reversed=True),
+                     {key1: {'sub3': 'val3'}, key2: {'sub3': 'val3'}})
+
+    def test_multiget_supercolumns(self):
+        key1 = 'TestSuerColumnFamily.test_multiget_supercolumns1'
+        key2 = 'TestSuerColumnFamily.test_multiget_supercolumns2'
+        keys = [key1, key2]
+        super1 = {'sub1': 'val1', 'sub2': 'val2'}
+        super2 = {'sub3': 'val3', 'sub4': 'val4'}
+        super3 = {'sub5': 'val5', 'sub6': 'val6'}
+        columns = {'1': super1, '2': super2, '3': super3}
+        scf.insert(key1, columns)
+        scf.insert(key2, columns)
+        assert_equal(scf.multiget(keys), {key1: columns, key2: columns})
+        assert_equal(scf.multiget(keys, columns=['1']),     {key1: {'1': super1}, key2: {'1': super1}})
+        assert_equal(scf.multiget(keys, column_start='3'),  {key1: {'3': super3}, key2: {'3': super3}})
+        assert_equal(scf.multiget(keys, column_finish='1'), {key1: {'1': super1}, key2: {'1': super1}})
+        assert_equal(scf.multiget(keys, column_count=1),    {key1: {'1': super1}, key2: {'1': super1}})
+        assert_equal(scf.multiget(keys, column_count=1, column_reversed=True), {key1: {'3': super3}, key2: {'3': super3}})
+
+    def test_get_range_super_column(self):
+        key = 'TestSuperColumnFamily.test_get_range_super_column'
+        subcolumns = {'sub1': 'val1', 'sub2': 'val2', 'sub3': 'val3'}
+        columns = {'1': subcolumns}
+        scf.insert(key, columns)
+        assert_equal(list(scf.get_range(start=key, finish=key, super_column='1')),
+                     [(key, subcolumns)])
+        assert_equal(list(scf.get_range(start=key, finish=key, super_column='1', columns=['sub1'])),
+                     [(key, {'sub1': 'val1'})])
+        assert_equal(list(scf.get_range(start=key, finish=key, super_column='1', column_start='sub3')),
+                     [(key, {'sub3': 'val3'})])
+        assert_equal(list(scf.get_range(start=key, finish=key, super_column='1', column_finish='sub1')),
+                     [(key, {'sub1': 'val1'})])
+        assert_equal(list(scf.get_range(start=key, finish=key, super_column='1', column_count=1)),
+                     [(key, {'sub1': 'val1'})])
+        assert_equal(list(scf.get_range(start=key, finish=key, super_column='1', column_count=1, column_reversed=True)),
+                     [(key, {'sub3': 'val3'})])
+
+    def test_get_range_super_columns(self):
+        key = 'TestSuperColumnFamily.test_get_range_super_columns'
+        super1 = {'sub1': 'val1', 'sub2': 'val2'}
+        super2 = {'sub3': 'val3', 'sub4': 'val4'}
+        super3 = {'sub5': 'val5', 'sub6': 'val6'}
+        columns = {'1': super1, '2': super2, '3': super3}
+        scf.insert(key, columns)
+        assert_equal(list(scf.get_range(start=key, finish=key, columns=['1'])),
+                     [(key, {'1': super1})])
+        assert_equal(list(scf.get_range(start=key, finish=key, column_start='3')),
+                     [(key, {'3': super3})])
+        assert_equal(list(scf.get_range(start=key, finish=key, column_finish='1')),
+                     [(key, {'1': super1})])
+        assert_equal(list(scf.get_range(start=key, finish=key, column_count=1)),
+                     [(key, {'1': super1})])
+        assert_equal(list(scf.get_range(start=key, finish=key, column_count=1, column_reversed=True)),
+                     [(key, {'3': super3})])
+
+    def test_get_count_super_column(self):
+        key = 'TestSuperColumnFamily.test_get_count_super_column'
+        subcolumns = {'sub1': 'val1', 'sub2': 'val2', 'sub3': 'val3'}
+        columns = {'1': subcolumns}
+        scf.insert(key, columns)
+        assert_equal(scf.get_count(key, super_column='1'),                       3)
+        assert_equal(scf.get_count(key, super_column='1', columns=['sub1']),     1)
+        assert_equal(scf.get_count(key, super_column='1', column_start='sub3'),  1)
+        assert_equal(scf.get_count(key, super_column='1', column_finish='sub1'), 1)
+
+    def test_get_count_super_columns(self):
+        key = 'TestSuperColumnFamily.test_get_count_super_columns'
+        columns = {'1': {'sub1': 'val1'}, '2': {'sub2': 'val2'}, '3': {'sub3': 'val3'}}
+        scf.insert(key, columns)
+        assert_equal(scf.get_count(key),                    3)
+        assert_equal(scf.get_count(key, columns=['1']),     1)
+        assert_equal(scf.get_count(key, column_start='3'),  1)
+        assert_equal(scf.get_count(key, column_finish='1'), 1)
+
+    def test_multiget_count_super_column(self):
+        key1 = 'TestSuperColumnFamily.test_multiget_count_super_column'
+        key2 = 'TestSuperColumnFamily.test_multiget_count_super_column'
+        keys = [key1, key2]
+        subcolumns = {'sub1': 'val1', 'sub2': 'val2', 'sub3': 'val3'}
+        columns = {'1': subcolumns}
+        scf.insert(key1, columns)
+        scf.insert(key2, columns)
+        assert_equal(scf.multiget_count(keys, super_column='1'),                       {key1: 3, key2: 3})
+        assert_equal(scf.multiget_count(keys, super_column='1', columns=['sub1']),     {key1: 1, key2: 1})
+        assert_equal(scf.multiget_count(keys, super_column='1', column_start='sub3'),  {key1: 1, key2: 1})
+        assert_equal(scf.multiget_count(keys, super_column='1', column_finish='sub1'), {key1: 1, key2: 1})
+
+    def test_multiget_count_super_columns(self):
+        key1 = 'TestSuperColumnFamily.test_multiget_count_super_columns'
+        key2 = 'TestSuperColumnFamily.test_multiget_count_super_columns'
+        keys = [key1, key2]
+        columns = {'1': {'sub1': 'val1'}, '2': {'sub2': 'val2'}, '3': {'sub3': 'val3'}}
+        scf.insert(key1, columns)
+        scf.insert(key2, columns)
+        assert_equal(scf.multiget_count(keys),                    {key1: 3, key2: 3})
+        assert_equal(scf.multiget_count(keys, columns=['1']),     {key1: 1, key2: 1})
+        assert_equal(scf.multiget_count(keys, column_start='3'),  {key1: 1, key2: 1})
+        assert_equal(scf.multiget_count(keys, column_finish='1'), {key1: 1, key2: 1})
+
+    def test_batch_insert(self):
+        key1 = 'TestSuperColumnFamily.test_batch_insert1'
+        key2 = 'TestSuperColumnFamily.test_batch_insert2'
+        columns = {'1': {'sub1': 'val1'}, '2': {'sub2': 'val2', 'sub3': 'val3'}}
+        scf.batch_insert({key1: columns, key2: columns})
+        assert_equal(scf.get(key1), columns)
+        assert_equal(scf.get(key2), columns)
+
+    def test_remove(self):
+        key = 'TestSuperColumnFamily.test_remove'
+        columns = {'1': {'sub1': 'val1'}, '2': {'sub2': 'val2', 'sub3': 'val3'}, '3': {'sub4': 'val4'}}
+        scf.insert(key, columns)
+        assert_equal(scf.get_count(key), 3)
+        scf.remove(key, super_column='1')
+        assert_equal(scf.get_count(key), 2)
+        scf.remove(key, columns=['3'])
+        assert_equal(scf.get_count(key), 1)
+
+        assert_equal(scf.get_count(key, super_column='2'), 2)
+        scf.remove(key, super_column='2', columns=['sub2'])
+        assert_equal(scf.get_count(key, super_column='2'), 1)
