179a557b81f5699d6d4c5665f8a957744d471872
Tyler Hobbs <tyler@datastax.com>
2011-03-31 12:01:39 -0500

    Version bump to 1.0.7

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ use_setuptools()
 from setuptools import setup
 from distutils.cmd import Command
 
-__version_info__ = (1, 0, 6)
+__version_info__ = (1, 0, 7)
 __version__ = '.'.join([str(v) for v in __version_info__])
 
 long_description = """pycassa is a python client library for Apache Cassandra with the following features:
