c67e40af3b6f5bb1c7246ea68dd36e2fbd73ed4b
Tyler Hobbs <tyler@datastax.com>
2011-09-25 14:14:35 -0500

    Don't require ez_setup in setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,17 @@ try:
 except:
     has_subprocess = False
 
-from ez_setup import use_setuptools
-use_setuptools()
-from setuptools import setup
+try:
+    from ez_setup import use_setuptools
+    use_setuptools()
+except ImportError:
+    pass
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 from distutils.cmd import Command
 
 __version_info__ = (1, 2, 0)
