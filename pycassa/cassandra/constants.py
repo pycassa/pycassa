cf0a370b98902f7ee0daf95ab6bcfba4b6a3389c
Tyler Hobbs <tyler@datastax.com>
2011-11-03 23:34:02 -0500

    Initial 1.0 compatibility work

--- pycassa/cassandra/constants.py
+++ pycassa/cassandra/constants.py
@@ -1,7 +1,9 @@
 from pycassa.cassandra.c07.constants import VERSION as VERSION_07
 from pycassa.cassandra.c08.constants import VERSION as VERSION_08
+from pycassa.cassandra.c10.constants import VERSION as VERSION_10
 
-__all__ = ['CASSANDRA_07', 'CASSANDRA_08']
+__all__ = ['CASSANDRA_07', 'CASSANDRA_08', 'CASSANDRA_10']
 
 CASSANDRA_07 = VERSION_07
 CASSANDRA_08 = VERSION_08
+CASSANDRA_10 = VERSION_10
