7d3fd06884bc11b9e1c60250052a1abdb6e7d44d
Tyler Hobbs <tyler@datastax.com>
2012-02-04 11:24:36 -0600

    Remove connection from pycassa package import

--- pycassa/__init__.py
+++ pycassa/__init__.py
@@ -3,7 +3,6 @@ from pycassa.columnfamilymap import *
 from pycassa.types import *
 from pycassa.index import *
 from pycassa.pool import *
-from pycassa.connection import *
 from pycassa.system_manager import *
 
 from pycassa.cassandra.ttypes import ConsistencyLevel,\
