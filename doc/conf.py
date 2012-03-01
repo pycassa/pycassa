e7d24f84ce8adde0986ac6f88143422df7409b72
Tyler Hobbs <tyler@datastax.com>
2012-02-29 22:08:47 -0600

    Bump version to 1.5.0, changelog updates

--- doc/conf.py
+++ doc/conf.py
@@ -32,9 +32,9 @@ project = 'pycassa'
 # built documents.
 #
 # The short X.Y version.
-version = '1.4'
+version = '1.5'
 # The full version, including alpha/beta/rc tags.
-release = '1.4.0'
+release = '1.5.0'
 
 # List of documents that shouldn't be included in the build.
 unused_docs = []
