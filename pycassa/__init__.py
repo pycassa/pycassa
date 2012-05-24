dff6dee7f878565ca98deb87cf803a4c9066adfe
Zachary Huff <zach.huff.386@gmail.com>
2012-05-24 14:45:31 -0300

    Misssing AuthenticationException and AuthorizationException

--- pycassa/__init__.py
+++ pycassa/__init__.py
@@ -5,8 +5,8 @@ from pycassa.index import *
 from pycassa.pool import *
 from pycassa.system_manager import *
 
-from pycassa.cassandra.ttypes import ConsistencyLevel,\
-    InvalidRequestException, NotFoundException, UnavailableException,\
-    TimedOutException
+from pycassa.cassandra.ttypes import AuthenticationException,\
+    AuthorizationException, ConsistencyLevel, InvalidRequestException,\
+    NotFoundException, UnavailableException, TimedOutException
 
 from pycassa.logging.pycassa_logger import *
