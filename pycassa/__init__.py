92af27dca2ae6026bf6d63908c736b0d62cbcfcb
Tyler Hobbs <tyler@datastax.com>
2014-01-14 13:05:10 -0600

    Set post-release version

--- pycassa/__init__.py
+++ pycassa/__init__.py
@@ -10,5 +10,5 @@ from pycassa.cassandra.ttypes import AuthenticationException,\
 
 from pycassa.logging.pycassa_logger import *
 
-__version_info__ = (1, 11, 0)
+__version_info__ = (1, 11, 0, 'post')
 __version__ = '.'.join(map(str, __version_info__))
